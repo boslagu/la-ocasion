b852d131dc8a170911ce997d4d366a14d2955bc3
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-10 21:59:04 +0800

    Test new connection

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+/*<?php
 
     $host = "ec2-54-147-33-38.compute-1.amazonaws.com";
     $user = "rhkeojjsqvafai";
@@ -7,7 +7,7 @@
     $port = "5432";
     
     $db_handle = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password);
-echo $db_handle
+
     try{
       //Set DSN data source name
         $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
@@ -22,6 +22,20 @@ echo $db_handle
     catch (PDOException $e) {
       echo 'Connection failed: ' . $e->getMessage();
       echo "<script>console.log(". $e->getMessage() .")</script>";
+    }
+?>*/
+
+<?php
+
+    $host = "host=ec2-54-147-33-38.compute-1.amazonaws.com";
+    $user = "user=rhkeojjsqvafai";
+    $password = "password=9a47afa990330d719c42f76180006c71252e34bb02c1804e6b4b820459e0439b";
+    $dbname = "dbname=derc23d3bc11l0";
+    $port = "port=5432";
+
+    $connect= pg_connect( "$host $port $dbname $user $password"  );
+    if(!$connect){
+      echo "Error : Unable to open database\n";
     }
 ?>
 
