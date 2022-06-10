7061a786edac933a43e8eefe457b690138024d7a
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 07:43:42 +0800

    Fixed bug imports.

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 <head>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/v"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="profile/js/frame.js"></script>
     <link rel="stylesheet" type="text/css" href="profile/styles/style.css">
     <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
