8bb615793381dc5eddc9c816d9d987d0ca38fd2d
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 12:47:34 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,12 +2,12 @@
   $sql = "DELETE FROM o_services WHERE service_id = ". $_GET['id'] ."";
 
   echo "<script>alert(". $_GET['id'] .")</script>";
-  // $result = pg_query($connect, $sql);
-  // if (!$result)
-  // {
-  //     echo pg_last_error($connect);
-  //     exit;
-  // }else{
-  //   echo "Record inserted!";
-  // }
+  $result = pg_query($connect, $sql);
+  if (!$result)
+  {
+    echo pg_last_error($connect);
+    exit;
+  }else{
+    echo "Record deleted!";
+  }
 ?>
