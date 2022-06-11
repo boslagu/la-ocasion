4ce02fd8ab4d1b609dce3e81ae907505f16a2655
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 09:16:02 +0800

    Removed services in test.

--- test.php
+++ test.php
@@ -3,7 +3,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/frame.js"></script>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -67,35 +70,6 @@
         </div>
     </header>
 
-    <!-- Services Section -->
-    <section id="services">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Services</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row text-center">
-                <?php
-                    $services = pg_query($connect,"SELECT * FROM o_services ORDER BY position_id ASC");
-                    if (!$services)
-                    {
-                    echo pg_last_error($connect);
-                      exit;
-                    }
-
-                    while($row = pg_fetch_array($services))
-                    {
-                        echo '<div class="col-md-4">
-                            <h4 class="service-heading">'. $row['title'] .'</h4>
-                            <p class="text-muted">'. $row['description'] .'</p>
-                        </div>';
-                    }
-                ?>
-            </div>
-        </div>
-    </section>
     
 
     <!-- Team Section -->
