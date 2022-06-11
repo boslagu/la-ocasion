23e2eab96d0d4e1a95b5775cb62a2e2df25c1143
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 18:45:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -64,6 +64,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <title></title>
 </head>
 <body>
