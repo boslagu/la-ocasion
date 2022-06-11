3278cbfc9adf2a1e63f99248c1e87ab7b255b82a
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 12:46:05 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,13 @@
 <?php
- echo "entry";
+  $sql = "DELETE FROM o_services WHERE service_id = ". $_GET['id'] ."";
 
+  echo "<script>alert(". $_GET['id'] .")</script>";
+  // $result = pg_query($connect, $sql);
+  // if (!$result)
+  // {
+  //     echo pg_last_error($connect);
+  //     exit;
+  // }else{
+  //   echo "Record inserted!";
+  // }
 ?>
