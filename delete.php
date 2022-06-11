daafe8b390484f4f85c4fb12d131bcf8c1e81b3d
Rommel M Lagurin <rlagurin.bos@gmail.com>
2022-06-11 12:49:09 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
   $sql = "DELETE FROM o_services WHERE service_id = ". $_GET['id'] ."";
 
-  echo "<script>alert(". $_GET['id'] .")</script>";
   $result = pg_query($connect, $sql);
   if (!$result)
   {
     echo pg_last_error($connect);
     exit;
   }else{
-    echo "Record deleted!";
+    echo "<script>alert(Record deleted!)</script>";
+    header("Location: https://la-ocasion.herokuapp.com/config.php"); 
   }
 ?>
